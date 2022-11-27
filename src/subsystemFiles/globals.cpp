a4b1b4e7ae588fe55a725afa67dcdc8bd9e770e6
baylietheiler <95715201+baylietheiler@users.noreply.github.com>
2022-11-27 15:45:08 -0700

    open lab |:()

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,13 +1,13 @@
 #include "../../include/subsystemHeaders/globals.hpp"
 
-pros::Motor frontLeftDrive(7,pros::E_MOTOR_GEARSET_06,true,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor backLeftDrive(8,pros::E_MOTOR_GEARSET_06,true,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor frontRightDrive(9,pros::E_MOTOR_GEARSET_06,true,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor backRightDrive(6,pros::E_MOTOR_GEARSET_06,false,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intake(5,pros::E_MOTOR_GEARSET_06,false,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor frontLeftDrive(7,pros::E_MOTOR_GEARSET_36,true,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor backLeftDrive(8,pros::E_MOTOR_GEARSET_36,true,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor frontRightDrive(9,pros::E_MOTOR_GEARSET_36,true,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor backRightDrive(6,pros::E_MOTOR_GEARSET_36,false,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(5,pros::E_MOTOR_GEARSET_18,false,pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor flywheel_inclinator(6,pros::E_MOTOR_GEARSET_06,false,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor flywheel1(11,pros::E_MOTOR_GEARSET_06,true,pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor flywheel2(12,pros::E_MOTOR_GEARSET_06,true,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor flywheel1(11,pros::E_MOTOR_GEARSET_36,true,pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor flywheel2(12,pros::E_MOTOR_GEARSET_36,true,pros::E_MOTOR_ENCODER_COUNTS);
 
 pros::ADIDigitalOut piston1 ('A');
 
