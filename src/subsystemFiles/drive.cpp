b4742bca45d58f5a34ed995162e4f0ea707f8c6e
baylietheiler <95715201+baylietheiler@users.noreply.github.com>
2022-11-30 13:19:21 -0700

    11/30 |:()

--- src/subsystemFiles/drive.cpp
+++ src/subsystemFiles/drive.cpp
@@ -35,10 +35,18 @@ double avgDrivePosition(){
 
 //driver control
 void setDriveMotors(){
-    int leftJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+    int leftJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    int rightJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    //if(abs(leftJoystick>10)) leftJoystick = 0;
+    //if(abs(rightJoystick)<10) rightJoystick = 0;
+    setLeftDrive(leftJoystick);
+    setRightDrive(rightJoystick);
+}
+   /* int leftJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
     int rightJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     if(abs(leftJoystick)<10)leftJoystick=0;
     if(abs(rightJoystick)<10)rightJoystick=0;
     setLeftDrive(-leftJoystick+rightJoystick);
     setRightDrive(-leftJoystick-rightJoystick);
-}
+    */
+
